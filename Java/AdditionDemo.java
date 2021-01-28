56af0ff0291d14c9de767640b7141c81e9eef4b5
kedards25 <kedards25@gmail.com>
2021-01-28 18:54:02 +0530

    SQL and Java Demos

---
+++ Java/AdditionDemo.java
@@ -0,0 +1,11 @@
+class AdditionDemo
+{
+	public static void main(String[] args)
+	{
+		int num1,num2,ans;
+		num1=Integer.parseInt(args[0]);
+		num2=Integer.parseInt(args[1]);
+		ans=num1+num2;
+		System.out.println("Answer is "+ans);
+	}
+}
